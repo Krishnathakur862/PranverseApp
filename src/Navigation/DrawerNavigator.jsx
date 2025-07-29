c37a3e5595171fb9ad2accf96d3e8efdb7758ef6
Samruddhi Sarode <sarodesamruddhi3@gmail.com>
2025-07-29 11:42:59 +0530

    new code

--- src/Navigation/DrawerNavigator.jsx
+++ src/Navigation/DrawerNavigator.jsx
@@ -8,20 +8,30 @@ import MeditationScreen from '../screens/MeditationScreen';
 import MyCoursesScreen from '../screens/MyCoursesScreen';
 import MyBookingsScreen from '../screens/MyBookingScreen';
 import EditProfile from '../screens/EditProfile';
+import ProfileScreen from '../screens/ProfileScreen';
+import NotificationScreen from '../screens/NotificationsScreen';
+import SettingsScreen from '../screens/SettingsScreen';
 
 const Drawer = createDrawerNavigator();
 
 
 export default function DrawerStack() {
   return (
-    <Drawer.Navigator initialRouteName="Products">
+    <Drawer.Navigator initialRouteName="Profile">
+      <Drawer.Screen name="Profile" component={ProfileScreen} />
+      <Drawer.Screen name="EditProfile" component={EditProfile} />
       <Drawer.Screen name="Products" component={ProductsScreen} />
       <Drawer.Screen name="Services" component={ServicesScreen} />
       <Drawer.Screen name="Courses" component={CoursesScreen} />
       <Drawer.Screen name="Meditation" component={MeditationScreen} />
       <Drawer.Screen name="MyCourses" component={MyCoursesScreen} />
       <Drawer.Screen name="MyBookings" component={MyBookingsScreen} />
-       <Drawer.Screen name="EditProfile" component={EditProfile} />
+      <Drawer.Screen name="Notification" component={NotificationScreen} />
+      <Drawer.Screen name="Settings" component={SettingsScreen} />
+
+
+
+       
     </Drawer.Navigator>
   );
 }
